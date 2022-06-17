78bf07389d4ab66ce95c57f4310d926da429b1f5
dathoang <dathoang31082001@gmail.com>
2022-06-17 10:57:23 +0700

    update scss file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-import App from './App';
-
+import './assets/boxicons-2.0.7/css/boxicons.min.css';
+import Layout from './components/Layout';
+import './sass/index.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />
-
+  <Layout />
 );
 
